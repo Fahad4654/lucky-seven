f03ae553befe926bace614b841020cdbe9b1f7db
Fahad Kabir <kabirkaife@gmail.com>
2025-08-06 17:35:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -46,9 +46,8 @@ export default function AppSidebar() {
         <SidebarMenu>
           {menuItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href} passHref>
+              <Link href={item.href} asChild>
                 <SidebarMenuButton
-                  as="a"
                   isActive={pathname === item.href}
                   tooltip={{ children: item.label, side: "right" }}
                 >
