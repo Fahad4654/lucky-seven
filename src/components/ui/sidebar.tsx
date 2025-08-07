e3174ff2ca4be912c91e7f7b23bc0838d1f9b656
Fahad Kabir <kabirkaife@gmail.com>
2025-08-07 11:24:49 +0000

    in mobile display if the sidebar is open and user click outside of the s

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -11,7 +11,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent } from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetOverlay } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
@@ -196,6 +196,7 @@ const Sidebar = React.forwardRef<
     if (isMobile) {
       return (
         <Sheet open={openMobile} onOpenChange={setOpenMobile} {...props}>
+          <SheetOverlay className="bg-black/80" />
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
